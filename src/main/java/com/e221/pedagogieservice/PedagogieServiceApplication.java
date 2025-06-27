11e0ffc5e512c6a2710f6f9cb3a6e94e5a8afbdc
Cheikh10 <cheikhibrambengue90@gmail.com>
2025-06-27 02:09:08 +0000

    Second commit E221

--- src/main/java/com/e221/pedagogieservice/PedagogieServiceApplication.java
+++ src/main/java/com/e221/pedagogieservice/PedagogieServiceApplication.java
@@ -1,12 +1,7 @@
 package com.e221.pedagogieservice;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class PedagogieServiceApplication {
--- src/test/java/com/e221/pedagogie_service/PedagogieServiceApplicationTests.java
+++ src/test/java/com/e221/pedagogie_service/PedagogieServiceApplicationTests.java
@@ -1,9 +1,10 @@
 package com.e221.pedagogie_service;
 
+import com.e221.pedagogieservice.PedagogieServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PedagogieServiceApplication.class)
 class PedagogieServiceApplicationTests {
 
 	@Test
